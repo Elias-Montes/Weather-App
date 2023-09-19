774bfba788c1c9637753a1a5408cae1ea5d56a03
[Elias Montes] <[eliasmontes911@gmail.com]>
2023-09-19 11:29:18 -0700

    created vars for script

---
+++ script.js
@@ -0,0 +1,8 @@
+var weatherKey = '9f6c902fcc076c9bd25330204bc6b85e';
+var city = 'Los Angeles';
+var todaysDate = dayjs().format('dddd, MMMM, D YYYY');
+var dateandTime = dayjs().format('hh:m A');
+var actualHistoryEl = $('.actualSearchhistory');
+var searchHistory = [];
+var domFiveDayForecast = $('.fivedayForecast');
+var weatherCardcurrent = $('.weatherCard');
